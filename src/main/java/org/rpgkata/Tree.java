4842b523824f29524ebdb26742ed1687b5bc1e35
cla <claricerdallorto@gmail.com>
2020-12-09 17:38:08 +0000

    healing

--- src/main/java/org/rpgkata/Tree.java
+++ src/main/java/org/rpgkata/Tree.java
@@ -2,9 +2,14 @@ package org.rpgkata;
 
 public class Tree {
     public Tree(int initialHealth) {
+
     }
 
     public boolean isAlive() {
         return true;
+    }
+
+    public void takesDamage(int i) {
+        health = health - damage;
     }
 }
--- src/test/java/org/rpgkata/PropTest.java
+++ src/test/java/org/rpgkata/PropTest.java
@@ -11,4 +11,11 @@ public class PropTest {
         Assertions.assertTrue(tree.isAlive());
     }
 
+    @Test
+    public void testPropTakesDamage(){
+        Tree tree1 = new Tree(2000);
+        tree1.takesDamage(200);
+        assertEquals(800, tree1.getHealth());
+    }
+
 }
